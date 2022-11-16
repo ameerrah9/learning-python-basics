4a190e9a2cbb5ee2a44f43f8f1d124be906e621e
Ghameerah (Juh-meer-ruh) McCullers <59162870+ameerrah9@users.noreply.github.com>
2022-11-16 12:02:04 -0500

    Classes complete

--- Ch2 - Basics/loops_start.py
+++ Ch2 - Basics/loops_start.py
@@ -8,20 +8,34 @@ def main():
     x = 0
 
     # TODO: define a while loop
-
+    while(x < 5):
+        print(x)
+        x = x + 1
 
     # TODO: define a for loop
-
+    # We have an iterator
+    # Range going from 5 to 10
+    # Within the range but not including the 10
+    for x in range(5, 10):
+        print(x)
 
     # TODO: use a for loop over a collection
     days = ["Mon","Tue","Wed","Thu","Fri","Sat","Sun"]
-
-
+    for day in days:
+        print(day)
     # TODO: use the break and continue statements
-
-
+    # Break the execution of a loop if a condition is met
+    for x in range(5, 10):
+        if x == 7:
+            break # When x gets to 7 it will break the loop
+        if x % 2 == 0:
+            continue # Skip the rest of the loop and do the next iteration # Skipping whatever meets that condition
+        print(x)
+        
     # TODO: using the enumerate() function to get index 
-    
-  
+    days = ["Mon","Tue","Wed","Thu","Fri","Sat","Sun"]
+    for i, d in enumerate(days): # Enumerate will iterate over the collection and returns the index and the item
+        print(i, d)
+
 if __name__ == "__main__":
     main()
--- Ch2 - Basics/variables_start.py
+++ Ch2 - Basics/variables_start.py
@@ -1,37 +1,48 @@
-# 
+#
 # Example file for variables
 # LinkedIn Learning Python course by Joe Marini
 #
 
 
 # Basic data types in Python: Numbers, Strings, Booleans, Sequences, Dictionaries
-myint = 5
-myfloat = 13.2
-mystr = "This is a string"
-mybool = True
-mylist = [0, 1, "two", 3.2, False]
-mytuple = (0, 1, 2)
-mydict = {"one" : 1, "two" : 2}
+my_int = 5
+my_float = 13.2
+my_str = "This is a string"
+my_bool = True
+my_list = [0, 1, "two", 3.2, False]
+my_tuple = (0, 1, 2)
+my_dict = {"one" : 1, "two" : 2}
 
-print(myint)
-print(myfloat)
-print(mystr)
-print(mybool)
-print(mylist)
-print(mytuple)
-print(mydict)
+# print(my_int)
+# print(my_float)
+# print(my_str)
+# print(my_bool)
+# print(my_list)
+# print(my_tuple)
+# print(my_dict)
 
 # re-declaring a variable works
-
+my_int = "abc"
 # to access a member of a sequence type, use []
+# Sequence Type is List or Tuple
+print(my_list[1])
+print(my_tuple[2])
 
 # use slices to get parts of a sequence
+print(my_list[1:5])
+print(my_list[1:5:2])
 
 # you can use slices to reverse a sequence
-
+print(my_list[::-1])
 # dictionaries are accessed via keys
-
+print(my_dict["one"])
 # ERROR: variables of different types cannot be combined
 
 # Global vs. local variables in functions
+def someFunction():
+  global my_str
+  my_str = "def"
+  print(my_str)
 
+someFunction()
+print(my_str)
